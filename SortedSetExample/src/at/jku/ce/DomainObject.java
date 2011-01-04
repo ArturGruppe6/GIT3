c8331d132a041d9b167ef1fc517f5bac526d5fcf
Artur <herbert_huber1@gmx.at>
2011-01-04 18:51:04 +0100

    Aber auch Artur findet während seiner Entwicklungungen an Version 2 noch einen Fehler – für das „name“-Attribut fehlen noch die getter- und setter-Methoden.

--- SortedSetExample/src/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/at/jku/ce/DomainObject.java
@@ -15,6 +15,11 @@ public class DomainObject {
 
 	private String name;
 
+	public String getName() { return name; }
+
+	public void setName(String name) { this.name = name; }
+
+
 	
 	public DomainObject(String name, String comment)s {
 		
