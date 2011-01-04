bcd3ba6b252e29c5e5e5d39c65c6c8820b446cfd
Artur <herbert_huber1@gmx.at>
2011-01-04 17:41:44 +0100

    Artur stellt das repository zur verfügung.

---
+++ SortedSetExample/src/at/jku/ce/DomainObject.java
@@ -0,0 +1,19 @@
+package at.jku.ce;
+
+public class DomainObject {
+
+	public DomainObject() {
+		super();
+	}
+	
+	public String id;
+	
+	public DomainObject(String id) {
+		
+		super(); this.id = id;
+		
+	}
+	
+	
+
+}
